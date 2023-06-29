2a86c80f7ba22b3884d48ae2c13106842e23dc90
DhruvGour <dhruv250800@gmail.com>
2023-06-29 15:02:31 +0530

    [DhruvGour]-UC3

--- PayrollService.cs
+++ PayrollService.cs
@@ -6,10 +6,17 @@ namespace EmployeePayrollService
 {
     internal class PayrollService
     {
-        public int Columns_Id { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
-        public long Salary { get; set; }
+        public int Salary { get; set; }
         public DateTime StartDate { get; set; }
-        public int Note_Id { get; set; }
+        public string Gender { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public int Deduction { get; set; }
+        public int Taxable_Pay { get; set; }
+        public int Income_Tax { get; set; }
+        public int Net_Pay { get; set; }
     }
 }
